2677afbc16314df158e5633046babf17edb940ec
RAT <stupeni555@gmail.com>
2026-02-09 21:01:19 +0200

    Add initial UI (1)

---
+++ src/components/OpeningCrawl.jsx
@@ -0,0 +1,10 @@
+import {openingCrawl} from "./utils/constants.js";
+
+const OpeningCrawl = () => {
+    return (
+        <p className="far-galaxy">{openingCrawl}
+        </p>
+    );
+};
+
+export default OpeningCrawl;
